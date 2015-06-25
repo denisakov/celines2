a71f312172c1777805fe53ce54ed708380b2608d
Denis Isakov <denisakov@gmail.com>
2015-06-25 19:37:54 +0300

    Burger test 2

--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
     $("#nav-mobile").html($("#nav-main").html());
-    $("#nav-trigger span").click(function(){
+    $(".c-hamburger").click(function(){
         if ($("nav#nav-mobile ul").hasClass("expanded")) {
             $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
             $(this).removeClass("open");
