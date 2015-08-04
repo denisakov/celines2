81a1bfa5184e536fb75928373a8029c06de8fb3e
Denis Isakov <denisakov@gmail.com>
2015-08-05 01:31:06 +0300

    Gallery is up and running with dummy images

---
+++ js/portfolio.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var p = $("#gallery").portfolio({
+        enableKeyboardNavigation: true, // enable / disable keyboard navigation (default: true)
+        loop: true, // loop on / off (default: false)
+        easingMethod: 'easeOutQuint', // other easing methods please refer: http://gsgd.co.uk/sandbox/jquery/easing/
+        height: '400px', // gallery height
+        width: '100%', // gallery width in pixels or in percentage
+        lightbox: false, // dim off other images, highlights only currently viewing image
+        showArrows: false, // show next / prev buttons
+        logger: false, // for debugging purpose, turns on/off console.log() statements in the script
+        spinnerColor: '#F8DCA5', // Ajax loader color
+        offsetLeft: -4, // position left value for current image
+        opacityLightbox: '0.2' // opacity of other images which are not active
+    });
+   	p.init();
+});
