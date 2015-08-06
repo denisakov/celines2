898619156a4fd50b3214b33126f3caba0bf9e1a1
Denis Isakov <denisakov@gmail.com>
2015-08-06 16:08:04 +0300

    Got the hamburger working properly

--- js/toggle.js
+++ js/toggle.js
@@ -9,13 +9,8 @@ jQuery(document).ready(function(){
             $(this).addClass("open");
         }
     });
-    $("nav#nav-mobile ul").click(function(){
-        if ($("nav#nav-mobile ul").hasClass("expanded")) {
-            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
-            $(this).removeClass("open");
-        } else {
-            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
-            $(this).addClass("open");
-        }
+    $("nav#nav-mobile ul li").click(function(){
+        $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+        $(".c-hamburger").removeClass("is-active");
     });
 });
